8fc6c9e97fa05700c9ee205143a23d3f246310a5
tomracia <tomasz.racia@gmail.com>
2021-10-11 21:20:35 +0200

    Removed `SimpleStorage` contract, no longer in use

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const PreciousStoneToken = artifacts.require('./PreciousStoneToken.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(PreciousStoneToken);
 }
