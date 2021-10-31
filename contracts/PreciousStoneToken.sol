9f48ee826d91c1b7842a0cfbed8dce3273aec27e
tomracia <tomasz.racia@gmail.com>
2021-10-31 21:11:51 +0100

    Added comments to the contract using NatSpec format

--- contracts/PreciousStoneToken.sol
+++ contracts/PreciousStoneToken.sol
@@ -11,8 +11,7 @@ import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 
 /// @title Precious Stones Mint via PST - Precious Stone Non-Fungible Tokens
 /// @author Tomasz Racia
-/// @notice It is a market of precious stones based on NFTs
-/// @dev Based on predefined contracts and extensions with custom logic for precious stones market
+/// @notice Market of precious stones based on minted NFTs
 /// @custom:experimental This is an experimental contract
 contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
@@ -93,6 +92,10 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     items[sku].buyer.transfer(amountToRefund);
   }
 
+  /// @notice Constructor to create PST - PreciousStoneToken
+  /// @dev Use proper address of smart contract from https://docs.chain.link/docs/ethereum-addresses/
+  /// @param priceFeedAddress Address of smart contract used as price feed for ETH/USD
+  /// @return ERC721 Non-Fungible Token Contract
   constructor(address priceFeedAddress) ERC721('PreciousStoneToken', 'PST') {
     admins[msg.sender] = true;
 
@@ -103,6 +106,8 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     priceFeed = AggregatorV3Interface(priceFeedAddress);
   }
 
+  /// @notice Get latest price from pricing feed
+  /// @return Latest price and number of decimals used for precision
   function getLatestPrice() public view returns (int, uint8) {
     (, int price, , ,) = priceFeed.latestRoundData();
     uint8 decimals = priceFeed.decimals();
@@ -110,10 +115,16 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return (price, decimals);
   }
 
+  /// @notice Checks if an address has admin role
+  /// @param adminAddress Ethereum address to verify admin role
+  /// @return True of false
   function isAdmin(address adminAddress) public view returns (bool) {
     return admins[adminAddress] ? true : false;
   }
 
+  /// @notice Grants admin role for particular address
+  /// @param additionalAdminAddress Ethereum address to add admin role for
+  /// @return True if there was no error
   function addAdmin(address additionalAdminAddress) public onlyAdmins returns (bool) {
     admins[additionalAdminAddress] = true;
 
@@ -122,6 +133,9 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Removes admin role for particular address
+  /// @param adminAddress Ethereum address to remove admin role from
+  /// @return True if there was no error
   function removeAdmin(address adminAddress) public onlyAdmins returns (bool) {
     delete admins[adminAddress];
 
@@ -130,10 +144,17 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Checks if an address has supplier role
+  /// @param supplierAddress Ethereum address to verify supplier role
+  /// @return True of false
   function isSupplier(address payable supplierAddress) public view returns (bool) {
     return suppliers[supplierAddress].active ? true : false;
   }
 
+  /// @notice Adds a new supplier with custom name
+  /// @param supplierAddress Ethereum address of a new supplier
+  /// @param supplierName Custom name of a new supplier
+  /// @return True if there was no error
   function addSupplier(address payable supplierAddress, string memory supplierName) public onlyAdmins returns (bool) {
     suppliers[supplierAddress] = Supplier({
       supplierAddress: supplierAddress,
@@ -146,6 +167,9 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Activates supplier role for particular address
+  /// @param supplierAddress Ethereum address to activate supplier role for
+  /// @return True if there was no error
   function activateSupplier(address payable supplierAddress) public onlyAdmins returns (bool) {
     suppliers[supplierAddress].active = true;
 
@@ -154,6 +178,9 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Deactivates supplier role for particular address
+  /// @param supplierAddress Ethereum address to deactivate supplier role from
+  /// @return True if there was no error
   function deactivateSupplier(address payable supplierAddress) public onlyAdmins returns (bool) {
     suppliers[supplierAddress].active = false;
 
@@ -162,6 +189,10 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Adds a new item to the market
+  /// @param ipfsHash Hash from IPFS that contains meta data for the item
+  /// @param priceUsd Price in USD of the item
+  /// @return True if there was no error
   function addItem(string memory ipfsHash, uint priceUsd) public onlySuppliers returns (bool) {
     items[skuCount] = Item({
       sku: skuCount,
@@ -181,6 +212,11 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Calculates price of an item based on current ETH/USD
+  /// @param ethToUsdPrice ETH/USD from price feed
+  /// @param ethToUsdDecimals Number of decimals used for precision of USD/ETH from price feed
+  /// @param usdPrice Price in USD of the item
+  /// @return Price in wei
   function _getPriceInWei(int ethToUsdPrice, uint8 ethToUsdDecimals, uint usdPrice) internal pure returns (uint) {
     uint8 precision = 18;
     uint ethToUsdPricePrec = uint(ethToUsdPrice) * 10 ** (precision  - ethToUsdDecimals);
@@ -189,6 +225,8 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return usdPricePrec / ethToUsdPricePrec;
   }
 
+  /// @notice Provides available for purchase items in the market
+  /// @return List of available for purchase items added by active suppliers with calculated price (wei)
   function getAvailableItems() public view returns (Item[] memory) {
     Item[] memory _items = new Item[](skuCount);
     (int ethToUsdPrice, uint8 ethToUsdDecimals) = getLatestPrice();
@@ -205,6 +243,9 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return _items;
   }
 
+  /// @notice Purchasing of particular item and minting NFT for it
+  /// @param sku ID of an item
+  /// @return True if there was no error
   function buyItem(uint sku) public payable forSale(sku) getWeiPrice(sku) paidEnough(items[sku].priceWei) checkValue(sku) returns (bool) {
     items[sku].supplier.supplierAddress.transfer(items[sku].priceWei);
 
@@ -219,26 +260,36 @@ contract PreciousStoneToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownab
     return true;
   }
 
+  /// @notice Base URI used for tokens IDs combined with IPFS hash in this case
+  /// @return IPFS base URI
   function _baseURI() internal pure override returns (string memory) {
     return 'https://ipfs.io/ipfs/';
   }
 
+  /// @inheritdoc ERC721Enumerable
   function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
     super._beforeTokenTransfer(from, to, tokenId);
   }
 
+  /// @inheritdoc ERC721URIStorage
   function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
     super._burn(tokenId);
   }
 
+  /// @inheritdoc ERC721URIStorage
   function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
     return super.tokenURI(tokenId);
   }
 
+  /// @inheritdoc ERC721Enumerable
   function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
     return super.supportsInterface(interfaceId);
   }
 
+  /// @notice Minting NFT for purchased item
+  /// @param to Ethereum address of token owner
+  /// @param _tokenURI URI of the token to IPFS where metadata is stored
+  /// @return ID of created token
   function mintItem(address to, string memory _tokenURI) internal returns (uint) {
     _tokenIds.increment();
     uint256 id = _tokenIds.current();
